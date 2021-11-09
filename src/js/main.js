35bf1c64b9d5821fdb2aed0bf0b15513e1079f70
Stanislaw <stanislaw@Stanislaws-Air.fritz.box>
2021-11-09 20:41:25 +0100

    WIP refactoring

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,13 @@
-const transactionItems = document.querySelectorAll(`.transactions-list__item`);
-const transactionAdd = document.querySelector(`.transaction-add`);
-const transactionAddButton = document.querySelector(`.transaction-add__btn`);
+const transactionItems = document.querySelectorAll(`.transactions__item`);
+const transactionDialog = document.querySelector(`.transaction__dialog`);
+const transactionDialogButton = document.querySelector(`.transaction__dialog-btn`);
 const transactionSubmitButton = document.querySelector(`.button`);
 
 const similarCardTemplate = document.querySelector(`#edit`).content.querySelector(`.transaction-edit`);
 
 transactionItems.forEach((item) => {
-  const transactionEditButton = item.querySelector(`.transactions-list__item--edit`);
-  const transactionCloseButton = item.querySelector(`.transactions-list__item--close`);
+  const transactionEditButton = item.querySelector(`.transactions__item--edit`);
+  const transactionCloseButton = item.querySelector(`.transactions__item--close`);
 
   item.onmouseover = function () {
     if (transactionCloseButton.classList.contains(`hidden`)) {
@@ -24,22 +24,22 @@ transactionItems.forEach((item) => {
   transactionEditButton.onclick = function (e) {
     transactionEditButton.classList.add(`hidden`);
     transactionItems.forEach((it) => {
-      it.classList.remove(`transactions-list__item--on-click`);
+      it.classList.remove(`transactions__item--on-click`);
       const card = document.querySelector(`.transaction-edit`);
       if (card) {
         card.remove();
       }
-      it.querySelector(`.transactions-list__item--close`).classList.add(`hidden`);
+      it.querySelector(`.transactions__item--close`).classList.add(`hidden`);
     });
     transactionCloseButton.classList.toggle(`hidden`);
-    item.classList.add(`transactions-list__item--on-click`);
+    item.classList.add(`transactions__item--on-click`);
 
     const parent = e.target.parentElement.parentElement;
 
     const cardElement = similarCardTemplate.cloneNode(true);
-    const sum = (parent.querySelector(`.transactions-list__item--expense`).textContent).slice(1, 3);
-    const category = (parent.querySelector(`.transactions-list__item-wrapper p`).textContent);
-    const account = (parent.querySelector(`.transactions-list__item--account`).textContent);
+    const sum = (parent.querySelector(`.transactions__item--expense`).textContent).slice(1, 3);
+    const category = (parent.querySelector(`.transactions__item-wrapper p`).textContent);
+    const account = (parent.querySelector(`.transactions__item--account`).textContent);
 
     cardElement.querySelector(`#sum`).value = sum;
     cardElement.querySelector(`#category`).value = category;
@@ -56,34 +56,34 @@ transactionItems.forEach((item) => {
       if (card) {
         card.remove();
       }
-      it.querySelector(`.transactions-list__item--edit`).classList.add(`hidden`);
-      it.querySelector(`.transactions-list__item--close`).classList.add(`hidden`);
+      it.querySelector(`.transactions__item--edit`).classList.add(`hidden`);
+      it.querySelector(`.transactions__item--close`).classList.add(`hidden`);
     });
-    item.classList.toggle(`transactions-list__item--on-click`);
+    item.classList.toggle(`transactions__item--on-click`);
   };
 });
 
-transactionAddButton.onclick = function () {
-  if (transactionAdd.classList.contains(`transaction-add__fade-in`)) {
-    transactionAdd.classList.toggle(`transaction-add__fade-in`);
-    transactionAdd.classList.toggle(`transaction-add__fade-out`);
-    transactionAddButton.classList.toggle(`transaction-add__btn--click`);
-    setTimeout(() => transactionAdd.classList.toggle(`transaction-add__hidden`), 900);
+transactionDialogButton.onclick = function () {
+  if (transactionDialog.classList.contains(`transaction__dialog--fade-in`)) {
+    transactionDialog.classList.toggle(`transaction__dialog--fade-in`);
+    transactionDialog.classList.toggle(`transaction__dialog--fade-out`);
+    transactionDialogButton.classList.toggle(`transaction__dialog-btn--click`);
+    setTimeout(() => transactionDialog.classList.toggle(`transaction__dialog--hidden`), 900);
   } else {
-    if (transactionAdd.classList.contains(`transaction-add__fade-out`)) {
-    transactionAdd.classList.toggle(`transaction-add__hidden`);
-    transactionAdd.classList.toggle(`transaction-add__fade-in`);
-    transactionAdd.classList.toggle(`transaction-add__fade-out`);
-    transactionAddButton.classList.toggle(`transaction-add__btn--click`);
+    if (transactionDialog.classList.contains(`transaction__dialog--fade-out`)) {
+    transactionDialog.classList.toggle(`transaction__dialog--hidden`);
+    transactionDialog.classList.toggle(`transaction__dialog--fade-in`);
+    transactionDialog.classList.toggle(`transaction__dialog--fade-out`);
+    transactionDialogButton.classList.toggle(`transaction__dialog-btn--click`);
   }
   }
 };
 
 transactionSubmitButton.onclick = function () {
   setTimeout(function () {
-    transactionAdd.classList.toggle(`transaction-add__fade-in`);
-    transactionAdd.classList.toggle(`transaction-add__fade-out`);
-    transactionAddButton.classList.toggle(`transaction-add__btn--click`);
-    setTimeout(() => transactionAdd.classList.toggle(`transaction-add__hidden`), 900);
+    transactionDialog.classList.toggle(`transaction__dialog--fade-in`);
+    transactionDialog.classList.toggle(`transaction__dialog--fade-out`);
+    transactionDialogButton.classList.toggle(`transaction__dialog--btn--click`);
+    setTimeout(() => transactionDialog.classList.toggle(`transaction__dialog--hidden`), 900);
   }, 500)
 };
